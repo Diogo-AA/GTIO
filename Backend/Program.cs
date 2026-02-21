2b4a8db085f8753ca2b515c295d28ad9cb566723
Diogo-AA <diogomanu91@protonmail.com>
2026-02-21 20:08:42 +0100

    doc: añadido documentación a los endpoints closes #15

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,10 +1,23 @@
 using Backend.Endpoints;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "API Votación", Version = "v1" });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Autorización",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "Introduce 'Bearer' seguido de un espacio y el JWT. Ejemplo: 'Bearer abc123'"
+    });
+});
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddValidation();
 
@@ -13,9 +26,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.MapAuthEndpoints();
 app.MapVotingEndpoints();
 
 app.Run();
