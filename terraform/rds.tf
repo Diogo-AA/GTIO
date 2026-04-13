159b2bccd1fcdcd56f821a7608ca03977f0fffc4
Cristian Meneses <75837804+DevcrewCM@users.noreply.github.com>
2026-04-14 01:28:24 +0200

    fix: añadir propiedad storage_encrypted a la instancia MySQL

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -11,6 +11,7 @@ resource "aws_db_instance" "mysql" {
   engine_version       = "8.0"
   instance_class       = "db.t3.micro" # Capa gratuita
   allocated_storage    = 20
+  storage_encrypted    = true
   db_name              = var.db_name
   username             = var.db_user
   password             = var.db_password 
