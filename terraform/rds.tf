e44b7b513d1622b99ff6910917abfad9bcbe1470
Cristian Meneses <75837804+DevcrewCM@users.noreply.github.com>
2026-04-14 01:31:08 +0200

    fix: definir periodo de retención de backups para RDS

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -12,6 +12,7 @@ resource "aws_db_instance" "mysql" {
   instance_class       = "db.t3.micro" # Capa gratuita
   allocated_storage    = 20
   storage_encrypted    = true
+  backup_retention_period = 7
   db_name              = var.db_name
   username             = var.db_user
   password             = var.db_password 
