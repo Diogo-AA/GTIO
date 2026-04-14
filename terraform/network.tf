1957a354e938198a6e9e8ce6cf2c066506b327c2
Cristian Meneses <meneses.185189@e.unavarra.es>
2026-04-14 23:24:40 +0200

    fix: refactorizar red, limpiar duplicados y abrir puerto SSH 22

---
+++ terraform/network.tf
@@ -0,0 +1,51 @@
+# network.tf - Base de red para el proyecto GTIO
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = { Name = "gtio-vpc" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "gtio-igw" }
+}
+
+# Subred Pública (Donde Diogo pondrá la EC2 con Kong/Backend)
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = { Name = "gtio-public-subnet" }
+}
+
+# Subredes Privadas (Donde Cristian pondrá el RDS)
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+  tags = { Name = "gtio-private-1" }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+  tags = { Name = "gtio-private-2" }
+}
+
+# Tabla de rutas para la subred pública
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
