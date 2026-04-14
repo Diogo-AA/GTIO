49e7c159066ffcc908268913ab5e9378fe74c11a
JavierTF <javiertoussentfis@gmail.com>
2026-04-14 23:05:14 +0200

    fix: añadir subredes privadas para RDS (perdidas al eliminar network.tf)

--- terraform/main.tf
+++ terraform/main.tf
@@ -151,3 +151,18 @@ resource "aws_instance" "app" {
     Name = "gtio-backend-instance"
   }
 }
+
+# Subredes privadas para RDS
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+  tags = { Name = "gtio-private-1" }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+  tags = { Name = "gtio-private-2" }
+}
