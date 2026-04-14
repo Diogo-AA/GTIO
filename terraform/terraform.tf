49e2d00d220e3d8ce18378e483f212b4e092cc8c
JavierTF <javiertoussentfis@gmail.com>
2026-04-14 23:01:07 +0200

    fix: corregir versiones de Terraform y provider AWS en terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "1.14.8"
+  required_version = ">= 1.9.0"
 
   cloud {
     organization = "GTIO"
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.40.0"
+      version = "~> 5.0"
     }
   }
 }
