049545fc2d4df4725afdc078f4a97348d4d74b97
JavierTF <javiertoussentfis@gmail.com>
2026-05-13 01:06:26 +0200

    front y back juntos

--- terraform/environments/pro/main.tf
+++ terraform/environments/pro/main.tf
@@ -37,11 +37,6 @@ module "compute" {
   desired_count = 2 # Alta disponibilidad en pro
 }
 
-module "frontend" {
-  source      = "../../modules/frontend"
-  environment = "pro"
-}
-
 module "observability" {
   source           = "../../modules/observability"
   environment      = "pro"
--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -41,6 +41,11 @@ variable "backend_image_tag" {
   default = "latest"
 }
 
+variable "frontend_image_tag" {
+  type    = string
+  default = "latest"
+}
+
 variable "desired_count" {
   type    = number
   default = 1
