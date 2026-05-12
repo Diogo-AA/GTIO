5f352005eaa0061910add3538dde1e28f97277dc
JavierTF <javiertoussentfis@gmail.com>
2026-05-12 23:52:12 +0200

    terraform front ok

--- terraform/environments/pro/outputs.tf
+++ terraform/environments/pro/outputs.tf
@@ -3,6 +3,21 @@ output "api_url" {
   value       = "http://${module.compute.alb_dns_name}"
 }
 
+output "frontend_url" {
+  description = "URL publica del frontend (CloudFront)"
+  value       = "https://${module.frontend.cloudfront_domain}"
+}
+
+output "frontend_bucket" {
+  description = "Bucket S3 donde subir el build del frontend"
+  value       = module.frontend.bucket_name
+}
+
+output "frontend_distribution_id" {
+  description = "ID de la distribucion CloudFront del frontend (para invalidaciones)"
+  value       = module.frontend.distribution_id
+}
+
 output "grafana_url" {
   description = "URL de Amazon Managed Grafana"
   value       = module.observability.grafana_url
